e24e6e89f8bd1daa57de8f645afcaf703797d3d7
Rohan Kotwani <rohankotwani@icloud.com>
2018-06-15 12:00:37 -0400

    Update kernelml-enhanced-ridge-example.py

--- kernelml-enhanced-ridge-example.py
+++ kernelml-enhanced-ridge-example.py
@@ -1,26 +1,25 @@
-import pandas as pd
-import time
+import kernelml
 import seaborn
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn import linear_model
-import kernelml
 
 train=pd.read_csv("data/kc_house_train_data.csv",dtype = {'bathrooms':float, 'waterfront':int, 'sqft_above':int, 'sqft_living15':float, 'grade':int, 'yr_renovated':int, 'price':float, 'bedrooms':float, 'zipcode':str, 'long':float, 'sqft_lot15':float, 'sqft_living':float, 'floors':str, 'condition':int, 'lat':float, 'date':str, 'sqft_basement':int, 'yr_built':int, 'id':str, 'sqft_lot':int, 'view':int})
 test=pd.read_csv("data/kc_house_test_data.csv",dtype = {'bathrooms':float, 'waterfront':int, 'sqft_above':int, 'sqft_living15':float, 'grade':int, 'yr_renovated':int, 'price':float, 'bedrooms':float, 'zipcode':str, 'long':float, 'sqft_lot15':float, 'sqft_living':float, 'floors':str, 'condition':int, 'lat':float, 'date':str, 'sqft_basement':int, 'yr_built':int, 'id':str, 'sqft_lot':int, 'view':int})
-
 
 def ridge_least_sqs_loss(x,y,w):
     alpha,w = w[-1][0],w[:-1]
     penalty = 0
     value = 1
     if alpha<value:
-        penalty = 1.5*abs(value-alpha)
+        penalty = 3*abs(value-alpha)
     if alpha<0:
         alpha=0
     hypothesis = x.dot(w)
     loss = hypothesis-y 
     return np.sum(loss**2)/len(y) + alpha*np.sum(w[1:]**2) + penalty*np.sum(w[1:]**2)
+
 
 X_train = train[['sqft_living','bedrooms','bathrooms']].values
 y_train = train[['price']].values
@@ -29,31 +28,60 @@ y_test = test[['price']].values
 SST_train = np.sum((y_train-np.mean(y_train))**2)
 SST_test = np.sum((y_test-np.mean(y_test))**2)
 
-start_time = time.time()
-model = kernelml.kernel_optimizer(X_train,y_train,ridge_least_sqs_loss,num_param=5)
-model.add_intercept()
+X_train = np.column_stack((np.ones(X_train.shape[0]),X_train))
+X_test = np.column_stack((np.ones(X_test.shape[0]),X_test))
 
-model.default_random_simulation_params(prior_uniform_low=1,prior_uniform_high=2)
-model.adjust_optimizer(update_magnitude=1000,n_parameter_updates=50)
-model.optimize()
-end_time = time.time()
-print("time:",end_time-start_time)
+runs = 2
+zscore = 2.0
+umagnitude = 0.01
+analyzenparam = 100
+nupdates = 50
+npriorsamples=40
+nrandomsamples = 600
+tinterations = 10
+sequpdate = False
+
+
+kml = kernelml.KernelML(
+         prior_sampler_fcn=None,
+         sampler_fcn=None,
+         intermediate_sampler_fcn=None,
+         parameter_transform_fcn=None,
+         batch_size=None)
+
+parameter_by_run = kml.optimize(X_train,y_train,loss_function=ridge_least_sqs_loss,
+                                num_param=5,
+                                args=[],
+                                runs=runs,
+                                total_iterations=tinterations,
+                                analyze_n_parameters=analyzenparam,
+                                n_parameter_updates=nupdates,
+                                update_magnitude=umagnitude,
+                                sequential_update=sequpdate,
+                                percent_of_params_updated=1,
+                                init_random_sample_num=npriorsamples,
+                                random_sample_num=nrandomsamples,
+                                convergence_z_score=1,
+                                prior_uniform_low=1,
+                                prior_uniform_high=2,
+                                plot_feedback=False,
+                                print_feedback=False)
 
 #Get model performance on validation data
-params = model.get_param_by_iter()
-errors = model.get_loss_by_iter()
-update_history = model.get_parameter_update_history()
+params = kml.model.get_param_by_iter()
+errors = kml.model.get_loss_by_iter()
+update_history = kml.model.get_parameter_update_history()
 w = params[np.where(errors==np.min(errors))].T
 alpha,w = w[-1][0],w[:-1]
 print('alpha:',alpha)
 print('w:',w)
 
-X = np.column_stack((np.ones(X_train.shape[0]),X_train))
-yp_train = X.dot(w)
+
+yp_train = X_train.dot(w)
 SSE_train = np.sum((y_train-yp_train)**2)
 
-X = np.column_stack((np.ones(X_test.shape[0]),X_test))
-yp_test = X.dot(w)
+
+yp_test = X_test.dot(w)
 SSE_test = np.sum((y_test-yp_test)**2)
 
 #Compare to sklearn.Ridge(alpha=1)
